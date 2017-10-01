2980893dd76d7b2c6116c40ed8e7db196b71c21f
ashleymarkfletcher <ashleymarkfletcher@gmail.com>
2017-10-01 20:48:24 +0100

    removed test

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const setDevice = require('./setDevice')
 const getDevices = require('./getDevices')
 
-getDevices().then((devices) => {
-  console.log('devices:', devices)
-})
-
 module.exports = {
   setDevice: setDevice,
   getDevices: getDevices
 }
 
-// example command
+// example set command
 // setDevice('Speakers')
 // .then(() => {
 // 	console.log('audio device switched!')
 // })
 // .catch((err) => {
 //   console.log('error switching audio device')
+// })
+
+// example get command
+// getDevices().then((devices) => {
+//   console.log('devices:', devices)
 // })
