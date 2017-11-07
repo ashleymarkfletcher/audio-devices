7061473892269fcdfad9c12ac9d412ebfaf89283
ashleymarkfletcher <ashleymarkfletcher@gmail.com>
2017-11-07 11:47:28 +0000

    added a bit of a hack to getdevices, added another case to the example

--- example.js
+++ example.js
@@ -1,6 +1,16 @@
-const audioDevices = require('audio-devices');
+// const audioDevices = require('audio-devices');
+const audioDevices = require('./index.js');
 
-audioDevices.setDevice('Speakers')
+// audioDevices.setDevice('Speakers')
+// .then(() => {
+//   console.log('done!')
+// })
+// .catch((err) => {
+//   console.log('error', error)
+// })
+
+
+audioDevices.getDevices()
 .then(() => {
   console.log('done!')
 })
--- getDevices.js
+++ getDevices.js
@@ -1,5 +1,9 @@
 const nircmd = require('nircmd')
 
 module.exports = () => {
-  return nircmd('showsounddevices')
+  // nircmd with showsounddevices never resolves?
+  nircmd('showsounddevices')
+
+  // HACK: just resolve for now
+  return Promise.resolve('')
 }
